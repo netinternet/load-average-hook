1fc71e7efd8b1e210ada7aa482ac0c92633b9cf9
Osman Makal <osman.makal@ni.net.tr>
2022-09-04 17:11:38 +0300

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+host=$1
+if [[ "$host" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+    GOOS=linux CGO_ENABLED=0 GOARCH=amd64 go build .
+    mv load-average-hook loadhook
+    ssh -t root@$host "systemctl stop loadhook"
+    scp ./loadhook root@$host:/usr/bin/loadhook 
+    scp ./loadhook.conf root@$host:/tmp/loadhook.conf 
+    ssh -t root@$host "[ ! -f /etc/loadhook.conf ] && cp -r /tmp/loadhook.conf /etc/loadhook.conf || rm -rf /tmp/loadhook.conf"
+    scp ./loadhook.service root@$host:/tmp/loadhook.service
+    ssh -t root@$host "[ ! -f /etc/systemd/system/loadhook.service ] && cp -r /tmp/loadhook.service /etc/systemd/system/loadhook.service || rm -rf /tmp/loadhook.service"
+    ssh -t root@$host "systemctl daemon-reload && systemctl restart waf && systemctl enable waf"
+else
+  echo "Example: bash deploy.sh 127.0.0.1"
+  exit 1 
+fi
+
